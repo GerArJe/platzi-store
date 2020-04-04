afe4ae565dfc2ab42574ba204f42e85404bfc011
Germán Arevalo <germanarevalojerez@gmail.com>
2020-04-04 10:16:47 -0500

    using routerlink,routeractive and creating product-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: HomeComponent
@@ -15,6 +22,10 @@ const routes: Routes = [
     component: ProductsComponent
   },
   {
+    path: 'products/:id',
+    component: ProductDetailComponent
+  },
+  {
     path: 'contact',
     component: ContactComponent
   },
@@ -22,6 +33,10 @@ const routes: Routes = [
     path: 'demo',
     component: DemoComponent
   },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
