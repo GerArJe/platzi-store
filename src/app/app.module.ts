4524eb273f3d28997bca29c0cda49ab10ebe4e9c
Germán Arevalo <germanarevalojerez@gmail.com>
2020-04-08 10:31:46 -0500

    HTTP Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/components/product/product.component';
-import { CartComponent } from './shared/components/cart/cart.component';
-import { ProductsComponent } from './product/components/products/products.component';
-import { ContactComponent } from './contact/components/contact/contact.component';
-import { DemoComponent } from './demo/components/demo/demo.component';
-import { PageNotFoundComponent } from './page-not-found/components/page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product/components/product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -27,7 +20,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     SharedModule,
     CoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
