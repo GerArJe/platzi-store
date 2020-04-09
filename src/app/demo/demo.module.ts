5a94dde5eb34184b19f5ae9207513b790d3decc9
Germán Arevalo <germanarevalojerez@gmail.com>
2020-04-09 09:10:10 -0500

    enviroment stag

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -3,9 +3,10 @@ import { DemoComponent } from './components/demo/demo.component';
 import { SharedModule } from '../shared/shared.module';
 import { DemoRoutingModule } from './demo-routing.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DemoComponent],
-  imports: [CommonModule, SharedModule, DemoRoutingModule],
+  imports: [CommonModule, SharedModule, DemoRoutingModule, FormsModule],
 })
 export class DemoModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +14,14 @@ import { MaterialModule } from '../material/material.module';
     HighlightDirective,
     HeaderComponent,
     FooterComponent,
+    CartComponent,
   ],
   exports: [
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
     FooterComponent,
+    CartComponent,
   ],
   imports: [CommonModule, RouterModule, MaterialModule],
 })
